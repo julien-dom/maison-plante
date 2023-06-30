1370f4dd0f75cbbb4aa320adbb59fe45484ba034
Julien Dominguez <dominguez.julien@gmail.com>
2023-06-30 14:14:44 +0200

    commit retirer cart

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,12 +2,40 @@ import { plantList } from '../datas/plantList'
 import PlantItem from './PlantItem'
 import '../styles/ShoppingList.css'
 
-function ShoppingList() {
+function ShoppingList({ cart, updateCart }) {
 	const categories = plantList.reduce(
 		(acc, plant) =>
 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
 		[]
 	)
+
+	function addToCart(name, price) {
+		const currentPlantSaved = cart.find((plant) => plant.name === name)
+		if (currentPlantSaved) {
+			const cartFilteredCurrentPlant = cart.filter(
+				(plant) => plant.name !== name
+			)
+			updateCart([
+				...cartFilteredCurrentPlant,
+				{ name, price, amount: currentPlantSaved.amount + 1 }
+			])
+		} else {
+			updateCart([...cart, { name, price, amount: 1 }])
+		}
+	}
+
+    const deleteFromCart = (name, price) => {
+        const currentPlantSaved = cart.find((plant) => plant.name === name);
+        if (currentPlantSaved.amount > 1) {
+          const updatedCart = cart.map((plant) =>
+            plant.name === name ? { ...plant, amount: plant.amount - 1 } : plant
+          );
+          updateCart(updatedCart);
+        } else {
+          const updatedCart = cart.filter((plant) => plant.name !== name);
+          updateCart(updatedCart);
+        }
+      };
 
 	return (
 		<div className='lmj-shopping-list'>
@@ -17,14 +45,18 @@ function ShoppingList() {
 				))}
 			</ul>
 			<ul className='lmj-plant-list'>
-				{plantList.map(({ id, cover, name, water, light }) => (
-					<PlantItem
-						key={id}
-						cover={cover}
-						name={name}
-						water={water}
-						light={light}
-					/>
+				{plantList.map(({ id, cover, name, water, light, price }) => (
+					<div key={id}>
+						<PlantItem
+							cover={cover}
+							name={name}
+							water={water}
+							light={light}
+							price={price}
+						/>
+						<button onClick={() => addToCart(name, price)}>Ajouter</button>
+                        <button onClick={() => deleteFromCart(name, price)}>Retirer</button>
+					</div>
 				))}
 			</ul>
 		</div>
